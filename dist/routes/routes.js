8ddedce6abe229274c221db41f2c427885b688fb
Fabiano <fabiano96@hotmail.com>
2019-06-02 23:53:48 -0300

    removido dist

--- dist/routes/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Routes = /** @class */ (function () {
-    function Routes() {
-    }
-    return Routes;
-}());
-exports.Routes = Routes;
